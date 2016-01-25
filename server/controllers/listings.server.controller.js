66a0ea77d66ce0db58e83c04b8caa601ffda5893
Justin Macedo <justinmacedo@ufl.edu>
2016-01-24 22:03:17 -0500

    finished assignment 4

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,4 +1,3 @@
-
 /* Dependencies */
 var mongoose = require('mongoose'), 
     Listing = require('../models/listings.server.model.js');
@@ -7,7 +6,6 @@ var mongoose = require('mongoose'),
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
   On an error you should send a 404 status code, as well as the error message. 
   On success (aka no error), you should send the listing(s) as JSON in the response.
-
   HINT: if you are struggling with implementing these functions, refer back to this tutorial 
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
@@ -48,8 +46,13 @@ exports.update = function(req, res) {
   var listing = req.listing;
 
   /* Replace the article's properties with the new properties found in req.body */
+  listing.code = req.body.code;
+  listing.name = req.body.name;
+  listing.address = req.body.address;
   /* save the coordinates (located in req.results if there is an address property) */
+  listing.coordinates = req.results;
   /* Save the article */
+  res.json(listing);
 };
 
 /* Delete a listing */
@@ -57,16 +60,28 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article */
+  console.log(listing);
+  Listing.remove(listing, function(err,list){
+    if (err){
+      throw err;
+    }
+    res.json(list);
+  });
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Your code here */
+  Listing.find({}, function(err,list){
+    if (err){
+      throw err;
+    }
+    res.json(list);
+  }).sort({code: 1});
 };
 
 /* 
   Middleware: find a listing by its ID, then pass it to the next request handler. 
-
   HINT: Find the listing using a mongoose query, 
         bind it to the request object as the property 'listing', 
         then finally call next
